8b8036809d3cdaf6207083c96543e48718803474
Anderlop3107 <anderlop3107@gmail.com>
2026-06-25 01:09:19 -0300

    style(driver): replace Mes SVG icon with a clean line-chart trend icon

--- pages/driver_balance.php
+++ pages/driver_balance.php
@@ -202,7 +202,7 @@ require __DIR__ . '/_header.php';
         Semana
     </button>
     <button type="button" class="segment-btn" onclick="changePeriod('mes', this)">
-        <svg style="width: 14px; height: 14px;" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2.5"><path stroke-linecap="round" stroke-linejoin="round" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 012-2h2a2 2 0 012 2v5a2 2 0 01-2 2h-2a2 2 0 01-2-2v-5zm-7 5a2 2 0 012-2h2a2 2 0 012 2v5a2 2 0 01-2 2h-2a2 2 0 01-2-2v-5z" /></svg>
+        <svg style="width: 14px; height: 14px;" fill="none" stroke="currentColor" viewBox="0 0 24 24" stroke-width="2.5"><path stroke-linecap="round" stroke-linejoin="round" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" /></svg>
         Mes
     </button>
 </div>
