3eb210599de311496a649758f442127cf117707b
Anderlop3107 <anderlop3107@gmail.com>
2026-08-13 01:14:25 -0300

    fix: sanitize api_get_order_live_location.php output buffer

--- pages/api_get_order_live_location.php
+++ pages/api_get_order_live_location.php
@@ -1,5 +1,8 @@
 <?php
+ob_start();
 require_once __DIR__ . '/../lib/app.php';
+
+ob_clean();
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
 header('Cache-Control: post-check=0, pre-check=0', false);
 header('Pragma: no-cache');
